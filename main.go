26f2460ee94540f9eb71cea045db43f9856aa7c4
raydive <raywassmas@gmail.com>
2018-08-18 23:43:59 +0900

    Added almost functions. Does it run? I don't know!!

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package cw2tt
 
 import (
 	"context"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/json"
 	"github.com/nulab/go-typetalk/typetalk/v1"
-	"golang.org/x/oauth2"
+	"io/ioutil"
 	"net/http"
-	"net/url"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -32,37 +35,72 @@ type webhook struct {
 }
 
 func sendToTypetalk(w http.ResponseWriter, r *http.Request) {
-	userAgent := r.Header.Get("User-Agent")
+	userAgent := r.UserAgent()
 	if !strings.Contains(userAgent, "ChatWork-Webhook") {
-		http.Error(w, "You don't have the permission. Only Chatwork Webhook", 401)
+		http.Error(w, "You don't have the permission. Only Chatwork Webhook", http.StatusUnauthorized)
+		return
 	}
-
-	client := makeTypetalkClient()
-	client.Messages.PostMessage(context.Background(), 1, "test", nil)
-}
-
-type AccessToken struct {
-	AccessToken  string `json:"access_token"`
-	TokenType    string `json:"token_type"`
-	ExpiresIn    int    `json:"expires_in"`
-	RefreshToken string `json:"refresh_token"`
-}
-
-func makeTypetalkClient() *v1.Client {
-	form := url.Values{}
-	form.Add("client_id", os.Getenv("CLIENT_ID"))
-	form.Add("client_secret", os.Getenv("CLIENT_SECRET"))
-	form.Add("grant_type", "client_credentials")
-	form.Add("scope", "topic.read,topic.post,topic.write,topic.delete,my")
-	oauth2resp, err := http.PostForm("https://typetalk.com/oauth2/access_token", form)
+	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		print("Client Credential request returned error")
+		http.Error(w, "Invalid body.", http.StatusBadRequest)
+		return
 	}
-	v := &AccessToken{}
-	json.NewDecoder(oauth2resp.Body).Decode(v)
-	tc := oauth2.NewClient(context.Background(), oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: v.AccessToken},
-	))
-	client := v1.NewClient(tc)
+
+	// Verify signature
+	signature := r.Header.Get("X-ChatWorkWebhookSignature")
+	if signature == "" {
+		http.Error(w, "You don't have X-ChatWorkWebhookSignature.", http.StatusUnauthorized)
+		return
+	}
+
+	token := os.Getenv("WEBHOOK_TOKEN")
+	digest, err := getDigest(token, body)
+	if err != nil {
+		http.Error(w, "Webhook token is missing. You should check server's environment variables.", http.StatusInternalServerError)
+		return
+	}
+
+	if digest != signature {
+		http.Error(w, "Invalid signature.", http.StatusUnauthorized)
+		return
+	}
+
+	// Take JSON data
+	data := webhook{}
+	err = json.Unmarshal(body, data)
+	if err != nil {
+		http.Error(w, "Request's body is not excepted JSON.", http.StatusBadRequest)
+		return
+	}
+
+	topicId, err := strconv.Atoi(os.Getenv("TOPIC_ID"))
+	if err != nil {
+		http.Error(w, "Invalid topic id. You should check server's environment variables.", http.StatusInternalServerError)
+		return
+	}
+
+	client := makeTypetalkBot()
+	_, _, err = client.Messages.PostMessage(context.Background(), topicId, data.WebhookEvent.Body, nil)
+	if err != nil {
+		http.Error(w, "We could not post to Typetalk. You should check server.", http.StatusInternalServerError)
+		return
+	}
+}
+
+func getDigest(token string, body []byte) (string, error) {
+	decodedToken, err := base64.StdEncoding.DecodeString(token)
+	if err != nil {
+		return "", err
+	}
+	mac := hmac.New(sha256.New, []byte(decodedToken))
+	mac.Write(body)
+	digest := base64.StdEncoding.EncodeToString(mac.Sum(nil))
+	return digest, nil
+}
+
+func makeTypetalkBot() *v1.Client {
+	token := os.Getenv("TYPETALK_TOKEN")
+	client := v1.NewClient(nil)
+	client.SetTypetalkToken(token)
 	return client
 }
